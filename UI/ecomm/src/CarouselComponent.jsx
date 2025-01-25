9ba064920862bc4686a06482535ff622c2105ef8
kunal <kunalsofficial21@gmail.com>
2025-01-25 11:47:45 +0530

    Disclaimer added

--- UI/ecomm/src/CarouselComponent.jsx
+++ UI/ecomm/src/CarouselComponent.jsx
@@ -19,6 +19,7 @@ const CarouselComponent = () => {
           <img src="https://images-eu.ssl-images-amazon.com/images/G/31/2024/Gateway/November/pc_1._CB542185732_.jpg" alt="Offer 1" />
           <div className="carousel-caption">
             <h2>Starting ₹5,799</h2>
+            <h3>⚠️ You are viewing an incomplete website because the AWS free tier has ended for the backend. </h3>
             <p>Mega offers on quick dry washing machines</p>
           </div>
         </div>
@@ -26,6 +27,8 @@ const CarouselComponent = () => {
           <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/PCwella_1._CB542131636_.png" alt="Offer 2" />
           <div className="carousel-caption">
             <h2>Extra up to ₹13,000 off</h2>
+                        <h3>⚠️ You are viewing an incomplete website because the AWS free tier has ended for the backend. </h3>
+
             <p>Exclusive bank offers on credit/debit cards</p>
           </div>
         </div>
@@ -33,6 +36,8 @@ const CarouselComponent = () => {
           <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/OnePlus/Nord/Nord4/30thOct/D143403581_Tall_Hero_3000x1200._CB542332614_.jpg" alt="Offer 3" />
           <div className="carousel-caption">
             <h2>Deals on Electronics</h2>
+                        <h3>⚠️ You are viewing an incomplete website because the AWS free tier has ended for the backend. </h3>
+
             <p>Best prices on top brands</p>
           </div>
         </div>
